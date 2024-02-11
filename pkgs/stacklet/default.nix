711f65d87da3f95b1738fbf261cc72e96eefde78
ggemre <ggemre+github@proton.me>
2024-02-11 13:31:08 -0700

    Update stacklet 1.0.0

--- pkgs/stacklet/default.nix
+++ pkgs/stacklet/default.nix
@@ -2,16 +2,16 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "stacklet";
-  version = "0.1.0";
+  version = "1.0.0";
 
   src = fetchFromGitHub {
     owner = "ggemre";
     repo = pname;
     rev = version;
-    sha256 = "0xxaphwrslp2s9sjk5crzp7himin9n6dynq5wd4b8pxcm5xa1mv3";
+    sha256 = "1jkc1na19wy5sicnj1w9cd8y55245dw7bvwmz1p9f43fjp5zv462";
   };
 
-  cargoSha256 = "sha256-RHQMcQZdOiRzatalEgth/lR7z65233dT5+M5NJZ+cmo=";
+  cargoSha256 = "sha256-jUn6mrTemaoa70Hac67UMlMPLVQpUvEu+vFCO+QD7mE=";
   buildInputs = [ ncurses ];
 
   meta = with lib; {
